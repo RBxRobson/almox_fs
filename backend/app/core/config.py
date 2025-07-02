f2335115726c0751c4975f6a95b78f61611e0319
RBxRobson <robsonnbarbosa0@gmail.com>
2025-07-02 15:31:23 -0300

    chore: Adiciona variáveis env no settings

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,7 +1,11 @@
 from pydantic_settings import BaseSettings
+from typing import Literal
 
 class Settings(BaseSettings):
     database_url: str
+    secret_key: str
+    algorithm: Literal["HS256", "RS256", "HS384", "HS512"]
+    access_token_expire_minutes: int
 
     class Config:
         env_file = ".env"
