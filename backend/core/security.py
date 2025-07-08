37c27a1192f919248f2c69164a3ebfc163fae307
RBxRobson <robsonnbarbosa0@gmail.com>
2025-07-08 15:29:17 -0300

    fix: Corrige erro no uso do timezone

--- backend/core/security.py
+++ backend/core/security.py
@@ -1,11 +1,14 @@
-from datetime import timezone, timedelta
+from datetime import datetime, timezone, timedelta
 from typing import Optional
 from jose import jwt, JWTError
 from core.config import settings
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
     to_encode = data.copy()
-    expire = timezone.utc() + (expires_delta or timedelta(minutes=settings.access_token_expire_minutes))
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=settings.access_token_expire_minutes))
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
     return encoded_jwt
@@ -16,3 +19,9 @@ def verify_access_token(token: str) -> Optional[dict]:
         return payload
     except JWTError:
         return None
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
