9628736bbdd113033de47d57f17e0068fa5cd9c6
RBxRobson <robsonnbarbosa0@gmail.com>
2025-07-18 16:06:39 -0300

    feat: Adiciona model category

--- backend/models/category.py
+++ backend/models/category.py
@@ -1,14 +1,18 @@
 import uuid
 from sqlalchemy import Column, String, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from core.database import Base  # ajuste conforme sua estrutura real
+from core.database import Base 
 
 class Category(Base):
     __tablename__ = "categories"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    name = Column(String, unique=True, nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
-    created_by = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    name = Column(String, nullable=False)
+    created_by = Column(UUID(as_uuid=True), ForeignKey("users.id"))
+    created_at = Column(DateTime, server_default=func.now())
+
+    creator = relationship("User", backref="categories")
+
 
