355adedfa641e823f884e66734d866f2c478b067
RBxRobson <robsonnbarbosa0@gmail.com>
2025-07-18 16:07:24 -0300

    feat: Adiciona nova rota categories

---
+++ backend/routes/categories.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+from typing import List
+
+from schemas.category import CategoryCreate, CategoryRead
+from services.category import (
+    create_category as create_category_service,
+    get_all_categories,
+)
+from services.user import get_current_user
+from core.database import get_db
+from models.user import User, UserRole
+
+router = APIRouter(
+    prefix="/categories",
+    tags=["Categories"]
+)
+
+
+@router.post("/", response_model=CategoryRead, status_code=status.HTTP_201_CREATED)
+def create_category(
+    data: CategoryCreate,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):  
+    return create_category_service(db, data, current_user)
+
+
+@router.get("/", response_model=List[CategoryRead])
+def list_categories(
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+     
+):
+    return get_all_categories(db)
--- backend/routes/main.py
+++ backend/routes/main.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from routes import users, auth
+from routes import users, auth, categories
 
 router = APIRouter()
 
 router.include_router(users.router)
 router.include_router(auth.router)
+router.include_router(categories.router)
