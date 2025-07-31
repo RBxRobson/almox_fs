b9b6f0f5a62118415920b568895883b9499feb26
RBxRobson <robsonnbarbosa0@gmail.com>
2025-07-31 16:26:40 -0300

    chore: Ajustes nos modelos, adiciona relacionamento

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,3 +1,5 @@
 from .category import Category
 from .user import User
 from .material import Material
+from .movement import Movement
+from .stock import Stock
--- backend/models/material.py
+++ backend/models/material.py
@@ -5,10 +5,13 @@ from sqlalchemy.sql import func
 import uuid
 
 from core.database import Base
-
+ 
 
 class Material(Base):
     __tablename__ = "materials"
+
+    movements = relationship("Movement", back_populates="item")
+    stock = relationship("Stock", back_populates="material", uselist=False)
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String(100), unique=True, nullable=False)
@@ -20,3 +23,4 @@ class Material(Base):
     creator = relationship("User", back_populates="materials_created")
 
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+    
