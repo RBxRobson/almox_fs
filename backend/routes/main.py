7f3cc1b06fc614899819a3d84617d50199a66b20
RBxRobson <robsonnbarbosa0@gmail.com>
2025-08-01 15:37:45 -0300

    feat: Adiciona novas rotas movement e stock

--- backend/routes/main.py
+++ backend/routes/main.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from routes import users, auth, categories, material
+from routes import users, auth, categories, material, stock, movement
 
 router = APIRouter()
 
@@ -7,3 +7,5 @@ router.include_router(users.router)
 router.include_router(auth.router)
 router.include_router(categories.router)
 router.include_router(material.router)
+router.include_router(stock.router)
+router.include_router(movement.router)
